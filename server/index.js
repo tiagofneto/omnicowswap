ab7d2224cdb52dad74e68c8d67a80b75e25e1e81
tiagofneto <tiagofneto29@gmail.com>
2022-07-23 16:55:56 +0200

    parse bundle

--- server/index.js
+++ server/index.js
@@ -8,12 +8,20 @@ app.listen(port, () => {
   console.log(`Listening on port ${port}`)
 })
 
+app.use((req, res, next) => {
+    res.append('Access-Control-Allow-Origin', ['*']);
+    res.append('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.append('Access-Control-Allow-Headers', 'Content-Type');
+    next();
+});
+
 const txs = []
 
 app.post('/order', (req, res) => {
   txs.push(req.body)
+  console.log(txs)
   res.end()
 })
 
 const bundleMinutes = 1
-setInterval(parseBundle, bundleMinutes * 60 * 1000)
+setInterval(() => parseBundle(txs) , bundleMinutes * 60 * 1000)
--- server/parse.js
+++ server/parse.js
@@ -1,7 +1,22 @@
-export function parseBundle() {
+import { ethers } from 'ethers'
+
+export function parseBundle(txs) {
+  const totalBalance = computeTotalBalance(txs)
+
+  users = txs.map(tx => tx.address)
+  tokens = txs.map(tx => tx.token)
+  amounts = txs.map(tx => tx.amount)
+
+  const buyOrSell = totalBalance < 0
+}
+
+function computeTotalBalance(txs) {
   let totalBalance = 0;
-  for (tx of txs) {
+  for (const tx of txs) {
     const mul = tx.buy ? 1 : -1
     totalBalance += tx.amount * mul
   }
+
+  return totalBalance;
 }
+
